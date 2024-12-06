4094c152376f74be57037fd410ae608b6a49074c
Snkenim <snekim288@gmail.com>
2024-12-06 21:22:44 +0800

    process

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ const Page = () => {
     // Only proceed if validation passed
     if (!errorMessage && !userErrorMessage) {
       try {
-        const response = await fetch('http://localhost:5000/api/signup', {
+        const response = await fetch('https://frontgram.onrender.com/', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
